fa6702d62a3a5b7f61627801e29612b8fa48ae1f
junsuPark <krpjs0508@gmail.com>
2023-07-13 20:16:42 +0900

    layout

--- app/src/main/java/app/junsu/androidserviceex/MainActivity.kt
+++ app/src/main/java/app/junsu/androidserviceex/MainActivity.kt
@@ -7,6 +7,7 @@ import androidx.appcompat.app.AppCompatActivity
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
 
         val intent = Intent(applicationContext, TestService::class.java)
         startService(intent)
