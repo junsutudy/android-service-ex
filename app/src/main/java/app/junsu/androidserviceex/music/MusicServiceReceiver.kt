8de288993de44a6f01fa7d985b39b719e39a57d6
junsuPark <krpjs0508@gmail.com>
2023-07-19 19:26:06 +0900

    rcv

---
+++ app/src/main/java/app/junsu/androidserviceex/music/MusicServiceReceiver.kt
@@ -0,0 +1,14 @@
+package app.junsu.androidserviceex.music
+
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+
+class MusicServiceReceiver : BroadcastReceiver() {
+    override fun onReceive(
+        context: Context?,
+        intent: Intent?,
+    ) {
+
+    }
+}
